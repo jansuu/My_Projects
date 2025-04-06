2d8afe95b6cfd06fafae4b49a1517ce06dab9d79
JASWANTH T <123795412+JaswanthT-21ec033@users.noreply.github.com>
2025-04-06 14:40:54 +0530

    Finance module inserted

---
+++ cozyheaven/src/main/java/com/hotelbooking/cozyheaven/CozyheavenApplication.java
@@ -0,0 +1,13 @@
+package com.hotelbooking.cozyheaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyheavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozyheavenApplication.class, args);
+	}
+
+}
---
+++ cozyheaven/src/test/java/com/hotelbooking/cozyheaven/CozyheavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelbooking.cozyheaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozyheavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
